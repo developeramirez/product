d297a187e90f3daa82d272e94345d3e468ba613f
Allan <allanramirez.edutec@gmail.com>
2019-05-15 11:48:33 -0600

    creacion de archivo insert

--- connection.php
+++ connection.php
@@ -1,1 +1,28 @@
-connection.php
+<?php 
+$servername= "localhost";
+$username= "root";
+$password = "captan123@*";
+$dbname = "products";
+
+
+$product_name = $_POST["product_name"];
+$price = $_POST["price"];
+$is_active = $_POST["is_active"];
+
+
+try{
+
+	$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username,$password);
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$sql = "INSERT INTO product (id,product_name,price,is_active)  values (null, '$product_name','$price','$is_active')";
+	$conn -> exec($sql);
+	echo "correcto";
+	header('Location: index.html');
+
+}catch(PDOException $e){
+	echo $sql . $e->getMessage();
+
+}
+$conn = null;
+
+ ?>
